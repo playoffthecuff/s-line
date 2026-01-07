f11e5608bd5e2a23aec9fdb42d7c29212b6de981
playoffthecuff <jmobile@bk.ru>
2026-01-07 20:24:13 +0300

    feat: add checkbox field component

---
+++ apps/website/src/app/components/checkbox-field/page.tsx
@@ -0,0 +1,169 @@
+import { CheckboxField } from "@/components/ui/fields/checkbox";
+import { HighlightedCode } from "@/components/ui/highlighted-code";
+import { Title } from "@/components/ui/typography/title";
+
+export default function CheckboxFieldPage() {
+	return (
+		<div
+			className="max-w-200 min-h-full bg-1 mx-auto p-18d flex flex-col gap-16d grow [&&]-pb-20"
+			style={{
+				width: "clamp(320px, 100%, 800px)",
+			}}
+		>
+			<div>
+				<Title>Checkbox Field</Title>
+				<p>
+					Labeled control, with an optional message and hint. Accepts boolean
+					data from the user.
+				</p>
+			</div>
+			<div>
+				<Title size={2}>Installation</Title>
+				<HighlightedCode
+					code="bunx @playoffthecuff/s3-line@latest add checkbox-field"
+					language="yaml"
+					lineNumbers={false}
+				/>
+			</div>
+			<hr className="mt-16d mb-12d" />
+			<Title size={2}>Variants:</Title>
+			<div className="flex flex-col gap-16d items-start w-60">
+				<div>
+					<Title size={3}>Options</Title>
+					<div className="flex flex-col gap-16d" >
+						<CheckboxField label="Only Label" id="ol" defaultChecked />
+						<CheckboxField label="Hint" hint="Hint" id="hint" defaultChecked />
+						<CheckboxField label="Required" required id="required" />
+						<CheckboxField label="With message" message="message" id="wm" defaultChecked />
+					</div>
+				</div>
+				<div>
+					<Title size={3}>Size</Title>
+					<div className="flex flex-col gap-16d">
+						<CheckboxField
+							label="Small"
+							message="S - Small"
+							size="s"
+							hint="Small"
+							id="small"
+						/>
+						<CheckboxField
+							label="Medium"
+							message="M - Medium"
+							size="m"
+							hint="Medium"
+							id="medium"
+						/>
+						<CheckboxField
+							label="Large"
+							message="L - large"
+							size="l"
+							hint="Large"
+							id="large"
+						/>
+					</div>
+				</div>
+				<div>
+					<Title size={3}>Appearance</Title>
+					<div className="flex flex-col gap-16d">
+						<CheckboxField
+							label="Outline"
+							message="outline"
+							hint="Outline"
+							id="outline"
+							defaultChecked
+						/>
+						<CheckboxField
+							label="Filled"
+							message="filled"
+							hint="Small"
+							id="filled"
+							filled
+							defaultChecked
+						/>
+					</div>
+				</div>
+				<div>
+					<Title size={3}>Status</Title>
+					<div className="flex flex-col gap-16d">
+						<CheckboxField
+							label="Valid"
+							message="valid"
+							hint="Valid"
+							status={"valid"}
+							id="valid"
+						/>
+						<CheckboxField
+							label="Warning"
+							message="warning"
+							hint="Warning"
+							status={"warning"}
+							id="warning"
+						/>
+						<CheckboxField
+							label="Invalid"
+							message="invalid"
+							hint="Invalid"
+							status={"invalid"}
+							id="invalid"
+						/>
+					</div>
+				</div>
+				<div>
+					<Title size={3}>Shape</Title>
+					<div className="flex flex-col gap-16d">
+						<CheckboxField
+							label="Square"
+							message="square"
+							hint="Square"
+							shape={"square"}
+							id="square"
+						/>
+						<CheckboxField
+							label="Rounded"
+							message="rounded"
+							hint="Rounded"
+							shape={"rounded"}
+							id="rounded"
+						/>
+						<CheckboxField
+							label="Circular"
+							message="circular"
+							hint="Circular"
+							shape={"circular"}
+							id="circular"
+						/>
+					</div>
+				</div>
+				<div>
+					<Title size={3}>State</Title>
+					<div className="flex flex-col gap-16d">
+						<CheckboxField
+							label="Label"
+							message="Disabled"
+							disabled
+							hint="Disabled"
+							id="disabled"
+							defaultChecked
+						/>
+						<CheckboxField
+							label="Label"
+							message="Loading"
+							loading
+							hint="Loading"
+							id="loading"
+						/>
+						<CheckboxField
+							label="Label"
+							message="Disable & Loading"
+							disabled
+							loading
+							hint="Disabled & Loading"
+							id="dnl"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ apps/website/src/components/ui/fields/checkbox.tsx
@@ -0,0 +1,65 @@
+import { cn } from "@/lib/utils/cn";
+import type { CheckboxProps } from "../checkbox/checkbox";
+import Checkbox from "../checkbox/checkbox";
+import { ErrorIcon } from "../icons/error";
+import { SuccessIcon } from "../icons/success";
+import { WarningIcon } from "../icons/warning";
+import { IntentMessage } from "../intent-message/intent-message";
+import { Label } from "../label/label";
+import { HintButton } from "./hint-button";
+
+type Props = CheckboxProps & { label: string; message?: string; hint?: string };
+
+const statusToIntent = {
+	valid: "success",
+	warning: "warning",
+	invalid: "error",
+} as const;
+
+const intentToIcon = {
+	success: SuccessIcon,
+	warning: WarningIcon,
+	error: ErrorIcon,
+};
+
+export function CheckboxField({
+	hint,
+	label,
+	message,
+	size,
+	required,
+	shape,
+	id,
+	...checkboxProps
+}: Props) {
+	const intent = checkboxProps.status
+		? statusToIntent[checkboxProps.status]
+		: undefined;
+	return (
+			<div className="grid grid-cols-[repeat(2,max-content)] gap-x-8d">
+				<Checkbox
+					size={size}
+					{...checkboxProps}
+					required={required}
+					shape={shape}
+					id={id}
+				/>
+				<div className="flex items-center gap-x-8d">
+					<Label intent={intent} size={size} required={required} htmlFor={id}>
+						{label}
+					</Label>
+					{hint && <HintButton description={hint} size={size} shape={shape} />}
+				</div>
+			{message && (
+				<IntentMessage
+					intent={intent}
+					size={size}
+					iconStart={intent ? intentToIcon[intent] : undefined}
+					className={cn("col-start-2", size === "m" ? "-translate-y-2" : size === "s" ? "-translate-y-[7px]" : "-translate-y-[10px]")}
+				>
+					{message}
+				</IntentMessage>
+			)}
+		</div>
+	);
+}
