42744f08e25a3ef2bd1346c373496cf115a03367
playoffthecuff <jmobile@bk.ru>
2026-01-07 20:50:56 +0300

    refactor: get started page

--- apps/website/src/app/get-started/page.tsx
+++ apps/website/src/app/get-started/page.tsx
@@ -1,21 +1,27 @@
 import Link from "next/link";
 import { HighlightedCode } from "@/components/ui/highlighted-code";
 import { defaultThemeConfig } from "./default-theme-config";
+import { Title } from "@/components/ui/typography/title";
 
 export default function Page() {
 	return (
-		<div className="max-w-200 mx-auto p-16d flex flex-col gap-16d">
-			<h1 className="text-8d font-11d">Get Started</h1>
-			<div className="relative">
+		<div
+			className="max-w-200 min-h-full bg-1 mx-auto p-18d flex flex-col gap-16d grow pb-20"
+			style={{
+				width: "clamp(320px, 100%, 800px)",
+			}}
+		>
+			<Title>Get Started</Title>
+			<div>
 				<p>Add regular dependencies:</p>
 				<HighlightedCode
-					code="bun add @base-ui-components/react class-variance-authority	clsx"
+					code="bun add @base-ui-components/react class-variance-authority clsx"
 					language="yaml"
 					lineNumbers={false}
 				/>
 			</div>
 
-			<div className="relative">
+			<div>
 				<p>Add dev dependencies:</p>
 				<HighlightedCode
 					code="bun add -D unocss @unocss/postcss @unocss/preset-wind4"
@@ -34,9 +40,9 @@ export default function Page() {
 					</a>
 				</p>
 			</div>
-			<div className="relative">
+			<div>
 				<p>
-					Configure <code className="bg-neutral-2 px-8d">tsconfig.json</code>:
+					Configure <code className="bg-2 px-8d">tsconfig.json</code>:
 				</p>
 				<HighlightedCode
 					code={`{
@@ -55,14 +61,13 @@ export default function Page() {
 						Customize the theme
 					</Link>{" "}
 					and use the generated configuration in the{" "}
-					<code className="bg-neutral-2 px-8d">uno.config.ts</code>
+					<code className="bg-2 px-8d">uno.config.ts</code>
 				</p>
 			</div>
-			<div className="relative">
+			<div>
 				<p>or use the default configuration:</p>
-				<div className="max-h-80 overflow-y-scroll">
-					<HighlightedCode code={defaultThemeConfig} language="js" />
-				</div>
+
+				<HighlightedCode code={defaultThemeConfig} language="js" />
 			</div>
 		</div>
 	);
