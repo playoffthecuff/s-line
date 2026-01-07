5a2f49b076852a9b0ce7c1a812aee99a9605a411
playoffthecuff <jmobile@bk.ru>
2026-01-07 20:28:44 +0300

    feat: add slider component

---
+++ apps/website/src/app/components/slider/page.tsx
@@ -0,0 +1,72 @@
+import { HighlightedCode } from "@/components/ui/highlighted-code";
+import { Slider } from "@/components/ui/slider/slider";
+import { Title } from "@/components/ui/typography/title";
+
+export default function SliderPage() {
+	return (
+		<div
+			className="max-w-200 min-h-full bg-1 mx-auto p-18d flex flex-col gap-16d grow [&&]-pb-20"
+			style={{
+				width: "clamp(320px, 100%, 800px)",
+			}}
+		>
+			<div>
+				<Title>Slider</Title>
+				<p>
+					An interactive control that accepts range's number(s) from the user.
+				</p>
+			</div>
+			<div>
+				<Title size={2}>Installation</Title>
+				<HighlightedCode
+					code="bunx @playoffthecuff/s3-line@latest add slider"
+					language="yaml"
+					lineNumbers={false}
+				/>
+			</div>
+			<hr className="mt-16d mb-12d" />
+			<Title size={2}>Variants:</Title>
+			<div className="flex flex-col gap-16d items-start">
+				<div className="flex flex-col gap-12d">
+					<Title size={3}>Type</Title>
+					<div className="grid grid-cols-[repeat(2,max-content)] gap-16d items-center">
+						<Slider defaultValue={50} /> Single
+						<Slider defaultValue={[25, 75]} /> Range
+					</div>
+				</div>
+				<div className="flex flex-col gap-12d">
+					<Title size={3}>Size</Title>
+					<div className="grid grid-cols-[repeat(2,max-content)] gap-16d items-center">
+						<Slider defaultValue={50} size={"s"}/> S - Small
+						<Slider defaultValue={50} size={"m"}/> M - Medium
+						<Slider defaultValue={50} size={"l"}/> L - Large
+					</div>
+				</div>
+				<div className="flex flex-col gap-12d">
+					<Title size={3}>Shape</Title>
+					<div className="grid grid-cols-[repeat(2,max-content)] gap-16d items-center">
+						<Slider defaultValue={50} shape={"square"} /> Square
+						<Slider defaultValue={50} shape={"rounded"} /> Rounded
+						<Slider defaultValue={50} shape={"circular"} /> L - Large
+					</div>
+				</div>
+				<div className="flex flex-col gap-12d">
+					<Title size={3}>Status</Title>
+					<div className="grid grid-cols-[repeat(2,max-content)] gap-16d items-center">
+						<Slider defaultValue={50} status={"valid"}/> Valid
+						<Slider defaultValue={50} status={"warning"}/> Warning
+						<Slider defaultValue={50} status={"invalid"}/> Invalid
+					</div>
+				</div>
+				<div className="flex flex-col gap-12d">
+					<Title size={3}>State</Title>
+					<div className="grid grid-cols-[repeat(2,max-content)] gap-16d items-center">
+						<Slider defaultValue={50} disabled/> Disabled
+						<Slider defaultValue={50} loading /> Loading
+						<Slider defaultValue={50} disabled loading/> Disabled & Loading
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ apps/website/src/components/ui/slider/slider.tsx
@@ -0,0 +1,163 @@
+import { Slider as BaseSlider } from "@base-ui-components/react/slider";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils/cn";
+import { SpinnerIcon } from "../icons/spinner";
+
+const sliderVariants = cva("flex grow items-center gap-14d px-[1ch] bg-3 min-w-28d", {
+	variants: {
+		size: {
+			s: "text-3d font-10d",
+			m: "text-4d font-9d",
+			l: "text-5d font-8d",
+		},
+		status: {
+			valid: "text-success",
+			warning: "text-warning",
+			invalid: "text-error",
+		},
+		shape: {
+			square: null,
+			rounded: "rounded-[0.325em]",
+			circular: "rounded-full",
+		},
+		disabled: {
+			true: "cursor-not-allowed",
+			false: null,
+		},
+		loading: {
+			true: "cursor-wait",
+			false: null,
+		},
+	},
+	compoundVariants: [
+		{
+			disabled: false,
+			loading: false,
+			className: "cursor-pointer",
+		},
+	],
+	defaultVariants: {
+		size: "m",
+		disabled: false,
+		loading: false,
+		shape: "rounded",
+	},
+});
+
+const controlVariants = cva(
+	"flex grow touch-none items-center h-[2em] select-none",
+	{
+		variants: {
+			disabled: {
+				true: "pointer-events-none saturate-50 opacity-50 contrast-60",
+			},
+			loading: {
+				true: "pointer-events-none",
+			},
+		},
+	},
+);
+
+const trackVariants = cva(null, {
+	variants: {
+		status: {
+			valid: "surface-success-solid-ia",
+			warning: "surface-warning-solid-ia",
+			invalid: "surface-error-solid-ia",
+		},
+		shape: {
+			square: null,
+			rounded: "rounded-[0.125em]",
+			circular: "rounded-full",
+		},
+	},
+	compoundVariants: [
+		{
+			status: undefined,
+			className: "surface-foreground-solid-ia",
+		},
+	],
+	defaultVariants: {
+		shape: "circular",
+	},
+});
+const thumbVariants = cva(
+	"flex items-center justify-center w-[2em] h-[2em] select-none  before-content-[''] before:w-[1em] before:h-[1em] before:block rounded-full before:rounded-full",
+	{
+		variants: {
+			status: {
+				valid:
+					"surface-success-ghost-ia before:surface-success-solid has-[:focus-visible]:before:shadow-focus-success",
+				warning:
+					"surface-warning-ghost-ia before:surface-warning-solid has-[:focus-visible]:before:shadow-focus-warning",
+				invalid:
+					"surface-error-ghost-ia before:surface-error-solid has-[:focus-visible]:before:shadow-focus-error",
+			},
+		},
+		compoundVariants: [
+			{
+				status: undefined,
+				className:
+					"surface-foreground-ghost-ia before:surface-foreground-solid has-[:focus-visible]:before:shadow-focus-primary",
+			},
+		],
+	},
+);
+
+export type SliderProps = BaseSlider.Root.Props &
+	VariantProps<typeof sliderVariants>;
+
+export function Slider({
+	size,
+	status,
+	shape,
+	disabled,
+	loading,
+	className,
+	...props
+}: SliderProps) {
+	const isRange =
+		(Array.isArray(props.defaultValue) && props.defaultValue.length) ||
+		(Array.isArray(props.value) && props.value.length);
+	return (
+		<BaseSlider.Root
+			{...props}
+			className={cn(
+				sliderVariants({ size, status, disabled, loading, shape }),
+				className,
+			)}
+			disabled={disabled}
+		>
+			<BaseSlider.Control className={controlVariants({ disabled, loading })}>
+				<BaseSlider.Track
+					className={cn(
+						"h-[0.375em] w-full surface-foreground-subtle-ia select-none",
+						trackVariants({ shape }),
+					)}
+				>
+					<BaseSlider.Indicator
+						className={cn("select-none", trackVariants({ status, shape }))}
+					/>
+					{[...Array(1 + +isRange)].map((_, i) => (
+						<BaseSlider.Thumb
+							tabIndex={loading ? -1 : 0}
+							className={thumbVariants({ status })}
+							index={i}
+							key={i}
+						/>
+					))}
+				</BaseSlider.Track>
+			</BaseSlider.Control>
+			<span className="relative flex items-center justify-center min-w-[1.25em]">
+				{loading && <SpinnerIcon className="absolute animate-spin" />}
+				<BaseSlider.Value
+					className={cn(
+						"justify-self-start",
+						loading && "text-transparent",
+						(disabled || loading) && "select-none",
+					)}
+				/>
+			</span>
+		</BaseSlider.Root>
+	);
+}
