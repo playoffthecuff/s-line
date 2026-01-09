8caecdaff22735c9a732b415d7d6408f3cc40f33
playoffthecuff <jmobile@bk.ru>
2026-01-09 05:40:07 +0300

    fix: add cli command

--- packages/cli/src/commands/add.ts
+++ packages/cli/src/commands/add.ts
@@ -1,17 +1,13 @@
 import inquirer from "inquirer";
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 import { registryFileFilesSchema, registryFileSchema, registryItemSchema, type RegistryFileFilesSchema, type RegistryFileSchema, type RegistryItemSchema } from "../schema/schema.js";
 import { installDepsIfMissing } from "./registry/deps.js";
-import { mkdirSync, copyFileSync, existsSync, writeFileSync } from 'node:fs';
+import { existsSync, writeFileSync } from 'node:fs';
 import { readFile } from "node:fs/promises";
 import { jsonParse } from "../utils/strings/json-parse.js";
 import z from "zod/v4";
 
 const UI_REGISTRY_URL = "https://smoothed.vercel.app/registry";
-
-const filePath = fileURLToPath(import.meta.url);
-const dirName = path.dirname(filePath);
 
 const getLocalRegistryEntry = async (dirname: string, filename: string) => {
 	const src = path.join(dirname, filename);
@@ -27,7 +23,7 @@ const getRemoteRegistryFile = async (url: string) => {
 	return registryFileSchema.parse(obj);
 };
 
-export const resolveLocalRegistryEntry =
+const resolveLocalRegistryEntry =
 	async (dirname: string, filename: string, handled = new Set<string>()): Promise<RegistryItemSchema[]> => {
 		if (handled.has(filename)) return [];
 		handled.add(filename);
@@ -42,7 +38,7 @@ export const resolveLocalRegistryEntry =
 		return [...deps, entry];
 	};
 
-export const resolveRemoteRegistryEntry =
+const resolveRemoteRegistryEntry =
 	async (url: string, handled = new Set<string>()): Promise<RegistryFileSchema[]> => {
 		const baseUrl = url.split(/(.+\/)[a-z]+\.json$/)[1];
 		if (!baseUrl) throw new Error(`Invalid URL ${url}`);
@@ -59,26 +55,25 @@ export const resolveRemoteRegistryEntry =
 		return [...deps, entry];
 	};
 
-const installRegistryFileContents = (files: RegistryFileFilesSchema, basePath: string) => {
+const installRegistryFileContents = (files: RegistryFileFilesSchema, cwd: string) => {
 	const parsedFiles = registryFileFilesSchema.safeParse(files);
 	if (!parsedFiles.success) throw new Error('Invalid object structure');
 	for (const file of parsedFiles.data) {
 		if (existsSync(file.path)) continue;
 		const { left, right } = jsonParse(file.content);
 		if (left) throw new Error('Invalid JSON content');
-		writeFileSync(path.join(basePath, file.path), right);
+		writeFileSync(path.join(cwd, file.path), right);
 	}
 };
 
-export const installRegistryFilesAndDeps = async (entries: Promise<RegistryFileSchema[]>, basePath: string) => {
-	const files = await entries;
-	const packagesToInstall = [...new Set(files.reduce((a: string[], v) => {
+export const installRegistryFilesAndDeps = async (entries: RegistryFileSchema[], cwd: string) => {
+	const packagesToInstall = [...new Set(entries.reduce((a: string[], v) => {
 		if (v.pkgDeps?.length) a.push(...v.pkgDeps);
 		return a;
 	}, []))];
 	installDepsIfMissing(packagesToInstall);
-	const filesToInstall = [...new Set(files.reduce((a: RegistryFileFilesSchema, v) => (a.push(...v.files), a), []))];
-	installRegistryFileContents(filesToInstall, basePath);
+	const filesToInstall = [...new Set(entries.reduce((a: RegistryFileFilesSchema, v) => (a.push(...v.files), a), []))];
+	installRegistryFileContents(filesToInstall, cwd);
 };
 
 const urlSchema = z.url();
@@ -91,19 +86,6 @@ export const add = async (name: string) => {
 	const normalizedName = name.trim().toLowerCase();
 	const parsedName = urlSchema.safeParse(normalizedName);
 	const url = parsedName.success ? parsedName.data : `${UI_REGISTRY_URL}/${normalizedName}.json`
-	const dir = path.join(process.cwd(), "src");
-	const registrySrc = path.join(dirName, "../src", "registry", "schema");
-	const templatesDir = path.join(dirName, "../src", "templates");
-	const registryItems = parsedName.success ? await resolveRemoteRegistryEntry(url) : await resolveLocalRegistryEntry(registrySrc, name.toLowerCase() + '.json');
-	for (const item of registryItems) {
-		if (item.pkgDeps?.length) await installDepsIfMissing(item.pkgDeps);
-		if (item.files.length) {
-			for (const file of item.files) {
-				const src = path.join(templatesDir, 'components', file.path);
-				const dest = path.join(dir, 'components/ui', file.path);
-				mkdirSync(path.dirname(dest), { recursive: true });
-				copyFileSync(src, dest);
-			}
-		}
-	}
+	const registryItems = await resolveRemoteRegistryEntry(url);
+	await installRegistryFilesAndDeps(registryItems, process.cwd());
 };
