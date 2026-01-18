8335f3239ffd5088533a7e48f8bdb2675a4de99d
playoffthecuff <jmobile@bk.ru>
2026-01-18 23:53:36 +0300

    reactor: add sizes to intent message component

--- apps/website/src/app/components/intent-message/page.tsx
+++ apps/website/src/app/components/intent-message/page.tsx
@@ -33,46 +33,51 @@ export default function InputPage() {
 			<div className="flex flex-col gap-16d">
 				<div className="flex flex-col gap-4d">
 					<Title size={3}>Size</Title>
-					<IntentMessage size={"s"}>Size S</IntentMessage>
-					<IntentMessage size={"m"}>Size M</IntentMessage>
-					<IntentMessage size={"l"}>Size L</IntentMessage>
+					<div className="flex flex-col gap-14d">
+						<IntentMessage size={"xs"}>Size Extra Small - XS</IntentMessage>
+						<IntentMessage size={"s"}>Size Small - S</IntentMessage>
+						<IntentMessage size={"m"}>Size Medium - M</IntentMessage>
+						<IntentMessage size={"l"}>Size Large - L</IntentMessage>
+						<IntentMessage size={"xl"}>Size Extra Large - XL</IntentMessage>
+					</div>
 				</div>
 				<div className="flex flex-col gap-4d">
 					<Title size={3}>Intent</Title>
-					<IntentMessage>Secondary Message</IntentMessage>
-
-					<IntentMessage intent="primary">Primary Message</IntentMessage>
-					<IntentMessage intent="warning">Warning Message</IntentMessage>
-
-					<IntentMessage intent={"success"}>Success Message</IntentMessage>
-
-					<IntentMessage intent={"error"}>Error Message</IntentMessage>
-					<IntentMessage intent={"visited"}>Visited Message</IntentMessage>
+					<div className="flex flex-col gap-14d">
+						<IntentMessage>Secondary Message</IntentMessage>
+						<IntentMessage intent="primary">Primary Message</IntentMessage>
+						<IntentMessage intent="warning">Warning Message</IntentMessage>
+						<IntentMessage intent={"success"}>Success Message</IntentMessage>
+						<IntentMessage intent={"error"}>Error Message</IntentMessage>
+						<IntentMessage intent={"visited"}>Visited Message</IntentMessage>
+					</div>
 				</div>
 				<div className="flex flex-col gap-4d">
 					<Title size={3}>With custom icon(s)</Title>
-					<IntentMessage iconStart={HelpIcon} iconEnd={HelpIcon}>
-						Secondary Message
-					</IntentMessage>
-					<IntentMessage intent="warning" iconStart={WarningIcon}>
-						Warning Message
-					</IntentMessage>
-					<IntentMessage intent={"success"} iconEnd={SuccessIcon}>
-						Success Message
-					</IntentMessage>
-					<IntentMessage
-						intent={"error"}
-						iconStart={ErrorIcon}
-						iconEnd={ErrorIcon}
-					>
-						Error Message
-					</IntentMessage>
-					<IntentMessage intent="primary" iconStart={InfoIcon}>
-						Primary Message
-					</IntentMessage>
-					<IntentMessage intent={"visited"} iconEnd={SparkleIcon}>
-						Visited Message
-					</IntentMessage>
+					<div className="flex flex-col gap-14d">
+						<IntentMessage iconStart={HelpIcon} iconEnd={HelpIcon}>
+							Secondary Message
+						</IntentMessage>
+						<IntentMessage intent="warning" iconStart={WarningIcon}>
+							Warning Message
+						</IntentMessage>
+						<IntentMessage intent={"success"} iconEnd={SuccessIcon}>
+							Success Message
+						</IntentMessage>
+						<IntentMessage
+							intent={"error"}
+							iconStart={ErrorIcon}
+							iconEnd={ErrorIcon}
+						>
+							Error Message
+						</IntentMessage>
+						<IntentMessage intent="primary" iconStart={InfoIcon}>
+							Primary Message
+						</IntentMessage>
+						<IntentMessage intent={"visited"} iconEnd={SparkleIcon}>
+							Visited Message
+						</IntentMessage>
+					</div>
 				</div>
 			</div>
 		</div>
--- apps/website/src/components/ui/intent-message/intent-message.tsx
+++ apps/website/src/components/ui/intent-message/intent-message.tsx
@@ -1,31 +1,36 @@
 import { cva, type VariantProps } from "class-variance-authority";
-import type { IconProps } from "../icons/types";
 import { cn } from "@/lib/utils/cn";
+import type { IconProps } from "../icons/types";
 
-export const intentMessageVariants = cva(
-	"h-[1.5em] flex items-center gap-[0.5ch]",
-	{
-		variants: {
-			intent: {
-				success: "text-success [&&]-selection:surface-success-solid",
-				error: "text-error [&&]-selection:surface-error-solid",
-				warning: "text-warning [&&]-selection:surface-warning-solid",
-				primary: "text-primary [&&]-selection:surface-primary-solid",
-				secondary: "text-secondary [&&]-selection:surface-secondary-solid",
-				visited: "text-visited [&&]-selection:surface-visited-solid",
-			},
-			size: {
-				s: "text-3d font-10d",
-				m: "text-4d font-9d",
-				l: "text-5d font-8d",
-			},
+export const intentMessageVariants = cva("flex items-center gap-[0.5ch]", {
+	variants: {
+		intent: {
+			success: "text-success [&&]-selection:surface-success-solid",
+			error: "text-error [&&]-selection:surface-error-solid",
+			warning: "text-warning [&&]-selection:surface-warning-solid",
+			primary: "text-primary [&&]-selection:surface-primary-solid",
+			secondary: "text-secondary [&&]-selection:surface-secondary-solid",
+			visited: "text-visited [&&]-selection:surface-visited-solid",
 		},
-		defaultVariants: {
-			size: "m",
-			intent: "secondary",
+		size: {
+			xs: "text-2d font-9d",
+			s: "text-3d font-9d",
+			m: "text-4d font-8d",
+			l: "text-5d font-7d",
+			xl: "text-6d font-7d",
 		},
 	},
-);
+	compoundVariants: [
+		{
+			size: ["l", "m", "s", "xl"],
+			className: "h-[0.84em]", //TODO migrate from text-#d to fs-#d with leading-none instead arbitrary height
+		},
+	],
+	defaultVariants: {
+		size: "m",
+		intent: "secondary",
+	},
+});
 
 const intentToColors = {
 	primary: "primary",
@@ -37,6 +42,12 @@ const intentToColors = {
 	visited: "visited",
 } as const;
 
+export type IntentMessageProps = React.ComponentProps<"p"> &
+	VariantProps<typeof intentMessageVariants> & {
+		iconStart?: React.ComponentType<IconProps>;
+		iconEnd?: React.ComponentType<IconProps>;
+	};
+
 export function IntentMessage({
 	intent,
 	size,
@@ -45,11 +56,7 @@ export function IntentMessage({
 	iconStart: IconStart,
 	iconEnd: IconEnd,
 	...props
-}: React.ComponentProps<"p"> &
-	VariantProps<typeof intentMessageVariants> & {
-		iconStart?: React.ComponentType<IconProps>;
-		iconEnd?: React.ComponentType<IconProps>;
-	}) {
+}: IntentMessageProps) {
 	const intentColor =
 		intentToColors[(intent ?? "secondary") as keyof typeof intentToColors];
 	return (
